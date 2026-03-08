ce3122e7bbc377a536f8d27e64df494f049d7284
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-03-08 13:53:32 +0400

    Styled and added basics funtionality

---
+++ script.js
@@ -0,0 +1,57 @@
+const formCon = document.getElementById("formCon");
+const input = document.getElementById("form__email-input"); 
+const subBtn = document.getElementById("subBtn");
+const errorMes = document.getElementById("errorMes");
+const successMes = document.getElementById("successMes");
+const userEmail = document.getElementById("userEmail");
+const successBtn = document.querySelector(".success__dismiss-btn");
+const loadingel = document.querySelector(".form__loading")
+
+subBtn.addEventListener("click", () => { 
+
+    let value = "";
+    const inputVal = input.value; 
+    value = inputVal;
+
+    loadingel.style.display = "block"
+    subBtn.style.display = "none"
+
+    setTimeout(() => {
+        if (value.includes("@") && value.includes("."))  { 
+            formCon.style.display = "none";
+            successMes.style.display = "flex";
+            userEmail.textContent = value;
+         }
+        else { 
+            errorMes.textContent = "Valid email required"
+                loadingel.style.display = "none"
+                subBtn.style.display = "block"
+        }       
+    }, 2000);
+});
+
+successBtn.addEventListener('click', () => { 
+    successMes.style.display = "none";
+    formCon.style.display = "flex";
+    input.value = "";
+    loadingel.style.display = "none";
+    subBtn.style.display = "inline-block";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
